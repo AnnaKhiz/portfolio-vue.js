869a5b880783ca0126fb4e871c08907284e0a6d3
annakhizhniak <kukurika.anna@gmail.com>
2024-05-02 23:58:17 +0300

    finished page cms

--- src/components/pages/CMSPage.vue
+++ src/components/pages/CMSPage.vue
@@ -1,10 +1,39 @@
 <template>
-	<div>CMS</div>
+	<div class="wrapper">
+        <main class="main">
+            <div class="container">
+                <div class="main__gallery">
+                    <div class="main__gallery-item">
+                <div class="main__gallery-item-img">
+                    <img :src="require('@/assets/img/meliorit.webp')" alt="">
+                </div>
+                <div class="main__gallery-textBlock">
+
+                    <h3 class="main__gallery-textBlock-label">
+                        -- WordPress --
+                    </h3>
+                    <p class="main__gallery-textBlock-info lng-descProjectWPMeliorit">
+                      {{ $t(`${this.checkedLanguage}.descProjectWPMeliorit`) }}
+                    </p>
+                    <a href="https://anna-khizhniak.site/wp/" class="main__gallery-textBlock-button lng-openProject">
+                      {{ $t(`${this.checkedLanguage}.openProject`) }}
+                    </a>
+                </div>
+            </div>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script>
   export default {
-    name: "CMSPage.vue"
+    name: "CMSPage.vue",
+    computed: {
+      checkedLanguage() {
+				return this.$i18next.language
+		},
+    }
   }
 </script>
 
