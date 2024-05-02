4498e09a8ceb046b962180253a3d74bbf743f9cb
annakhizhniak <kukurika.anna@gmail.com>
2024-05-03 00:05:03 +0300

    add alt to image

--- src/components/pages/CMSPage.vue
+++ src/components/pages/CMSPage.vue
@@ -4,22 +4,22 @@
             <div class="container">
                 <div class="main__gallery">
                     <div class="main__gallery-item">
-                <div class="main__gallery-item-img">
-                    <img :src="require('@/assets/img/meliorit.webp')" alt="">
-                </div>
-                <div class="main__gallery-textBlock">
+                      <div class="main__gallery-item-img">
+                          <img :src="require('@/assets/img/meliorit.webp')" alt="preview image">
+                      </div>
+                      <div class="main__gallery-textBlock">
 
-                    <h3 class="main__gallery-textBlock-label">
-                        -- WordPress --
-                    </h3>
-                    <p class="main__gallery-textBlock-info lng-descProjectWPMeliorit">
-                      {{ $t(`${this.checkedLanguage}.descProjectWPMeliorit`) }}
-                    </p>
-                    <a href="https://anna-khizhniak.site/wp/" class="main__gallery-textBlock-button lng-openProject">
-                      {{ $t(`${this.checkedLanguage}.openProject`) }}
-                    </a>
-                </div>
-            </div>
+                          <h3 class="main__gallery-textBlock-label">
+                              -- WordPress --
+                          </h3>
+                          <p class="main__gallery-textBlock-info lng-descProjectWPMeliorit">
+                            {{ $t(`${this.checkedLanguage}.descProjectWPMeliorit`) }}
+                          </p>
+                          <a href="https://anna-khizhniak.site/wp/" class="main__gallery-textBlock-button lng-openProject">
+                            {{ $t(`${this.checkedLanguage}.openProject`) }}
+                          </a>
+                      </div>
+                  </div>
                 </div>
             </div>
         </main>
